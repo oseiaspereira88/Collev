c4466aca54bff9283a9ac31b8a675aa06db0ac02
Oseias Pereira <oseiaspereira88@gmail.com>
2021-07-29 04:36:43 -0300

    Adicionando animações e OnClickListener aos itens dos PerfilFragments

--- app/src/main/java/com/empreendapp/collev/fragments/CategoriaPerfilFragment.java
+++ app/src/main/java/com/empreendapp/collev/fragments/CategoriaPerfilFragment.java
@@ -1,13 +1,17 @@
 package com.empreendapp.collev.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.empreendapp.collev.R;
 
 public class CategoriaPerfilFragment extends Fragment {
@@ -23,6 +27,40 @@ public class CategoriaPerfilFragment extends Fragment {
     }
 
     public void initViews(View rootView){
-        //gridView = (GridView) rootView.findViewById(R.id.notifiGrid);
+        CardView cv_option_1 = (CardView) rootView.findViewById(R.id.cv_option_1);
+        CardView cv_option_2 = (CardView) rootView.findViewById(R.id.cv_option_2);
+
+        cv_option_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateButton(v);
+                if(cv_option_2.getCardBackgroundColor().getDefaultColor() != 99){
+                    cv_option_1.setCardBackgroundColor(99);
+                } else{
+                    cv_option_2.setCardBackgroundColor(Color.WHITE);
+                    cv_option_1.setCardBackgroundColor(99);
+                }
+            }
+        });
+
+        cv_option_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateButton(v);
+                if(cv_option_1.getCardBackgroundColor().getDefaultColor() != 99){
+                    cv_option_2.setCardBackgroundColor(99);
+                } else{
+                    cv_option_1.setCardBackgroundColor(Color.WHITE);
+                    cv_option_2.setCardBackgroundColor(99);
+                }
+            }
+        });
+    }
+
+    public void animateButton(View view) {
+        YoYo.with(Techniques.Pulse)
+                .duration(700)
+                .repeat(0)
+                .playOn(view);
     }
 }
--- app/src/main/java/com/empreendapp/collev/fragments/DemandaPerfilFragment.java
+++ app/src/main/java/com/empreendapp/collev/fragments/DemandaPerfilFragment.java
@@ -1,12 +1,16 @@
 package com.empreendapp.collev.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.empreendapp.collev.R;
 
 public class DemandaPerfilFragment extends Fragment {
@@ -22,6 +26,40 @@ public class DemandaPerfilFragment extends Fragment {
     }
 
     public void initViews(View rootView){
-        //gridView = (GridView) rootView.findViewById(R.id.notifiGrid);
+        CardView cv_option_1 = (CardView) rootView.findViewById(R.id.cv_option_1);
+        CardView cv_option_2 = (CardView) rootView.findViewById(R.id.cv_option_2);
+
+        cv_option_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateButton(v);
+                if(cv_option_2.getCardBackgroundColor().getDefaultColor() != 99){
+                    cv_option_1.setCardBackgroundColor(99);
+                } else{
+                    cv_option_2.setCardBackgroundColor(Color.WHITE);
+                    cv_option_1.setCardBackgroundColor(99);
+                }
+            }
+        });
+
+        cv_option_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateButton(v);
+                if(cv_option_1.getCardBackgroundColor().getDefaultColor() != 99){
+                    cv_option_2.setCardBackgroundColor(99);
+                } else{
+                    cv_option_1.setCardBackgroundColor(Color.WHITE);
+                    cv_option_2.setCardBackgroundColor(99);
+                }
+            }
+        });
+    }
+
+    public void animateButton(View view) {
+        YoYo.with(Techniques.Pulse)
+                .duration(700)
+                .repeat(0)
+                .playOn(view);
     }
 }
