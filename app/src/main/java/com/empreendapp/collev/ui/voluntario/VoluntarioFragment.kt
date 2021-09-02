5ad3a7e7e906b125c1c6bdb7c8d0ca5b3d3ffce1
Oseias Pereira <oseiaspereira88@gmail.com>
2021-09-01 21:01:09 -0300

    Melhorias na tela principal do voluntario 3

--- app/src/main/java/com/empreendapp/collev/ui/voluntario/VoluntarioFragment.kt
+++ app/src/main/java/com/empreendapp/collev/ui/voluntario/VoluntarioFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import com.empreendapp.collev.R
 
 class VoluntarioFragment : Fragment() {
+    var imgSolicitarColeta: ImageView? = null
+    var cvCapa: CardView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voluntario, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_voluntario, container, false)
+        initViews(rootView);
+        return rootView
+    }
+
+    private fun initViews(rootView: View?) {
+        if (rootView != null) {
+            imgSolicitarColeta = rootView.findViewById(R.id.imgSolicitarColeta)
+            cvCapa = rootView.findViewById(R.id.cvCapa)
+
+            //adding all listeners
+            imgSolicitarColeta?.setOnClickListener(View.OnClickListener {
+                cvCapa?.visibility = View.GONE
+            })
+        }
     }
 }
