791fdd45636a91a2fe4cd9b1ec99b5d4f30bda50
Oseias Pereira <oseiapereira88@gmail.com>
2021-09-26 04:11:42 -0300

    implementada a função cancelar formulário de solicitação de coleta em VoluntarioFragment.kt

--- app/src/main/java/com/empreendapp/collev/ui/voluntario/VoluntarioFragment.kt
+++ app/src/main/java/com/empreendapp/collev/ui/voluntario/VoluntarioFragment.kt
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.daimajia.androidanimations.library.Techniques
@@ -18,29 +17,31 @@ import com.daimajia.androidanimations.library.YoYo
 import com.empreendapp.collev.R
 import com.empreendapp.collev.model.Coleta
 import com.google.firebase.auth.FirebaseAuth.getInstance
-import android.widget.TimePicker
 import android.app.TimePickerDialog;
+import android.widget.Toast
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
 
-open class VoluntarioFragment :  Fragment()  {
-    var imgSolicitarColeta: ImageView? = null
-    var clSolicitarColeta: ConstraintLayout? = null
-    var clChecks: ConstraintLayout? = null
-    var tvInfoCreate: TextView? = null
-    var tvDataColeta: TextView? = null
-    var tvDiaColeta: TextView? = null
-    var imgMapBG: ImageView? = null
-    var cvCapa: CardView? = null
-    var clColeta: ConstraintLayout? = null
-    var tvSpacer: TextView? = null
-    var itensSemana: ArrayList<ImageView>? = null
-    var itemSemanaSelectedPosition: Int? = null
-    var isStateCreate: Boolean? = false
-    var isStateCreated: Boolean? = false
-    private lateinit var button_clear: Button
+open class VoluntarioFragment : Fragment() {
+    private var imgSolicitarColeta: ImageView? = null
+    private var clSolicitarColeta: ConstraintLayout? = null
+    private var clChecks: ConstraintLayout? = null
+    private var tvInfoCreate: TextView? = null
+    private var tvDataColeta: TextView? = null
+    private var tvDiaColeta: TextView? = null
+    private var imgMapBG: ImageView? = null
+    private var cvCapa: CardView? = null
+    private var tvCapaInfo: TextView? = null
+    private var clColeta: ConstraintLayout? = null
+    private var tvSpacer: TextView? = null
+    private var itensSemana: ArrayList<ImageView>? = null
+    private var itemSemanaSelectedPosition: Int? = null
+    private var isStateCreate: Boolean? = false
+    private var isStateCreated: Boolean? = false
+    private var isCanceled: Boolean? = false
+    private lateinit var imgCancelForm: ImageView
     private lateinit var mContext: Context
     private lateinit var time: ImageView
     private lateinit var horaInicial: TextView
@@ -66,9 +67,10 @@ open class VoluntarioFragment :  Fragment()  {
             tvDiaColeta = rootView.findViewById(R.id.tvColetaRecipiente)
             imgMapBG = rootView.findViewById(R.id.imgMapBG)
             cvCapa = rootView.findViewById(R.id.cvCapa)
+            tvCapaInfo = rootView.findViewById(R.id.tvCapaInfo)
             clColeta = rootView.findViewById(R.id.clColeta)
             tvSpacer = rootView.findViewById(R.id.tvSpacer)
-            button_clear = rootView.findViewById(R.id.button_clear_clColeta)
+            imgCancelForm = rootView.findViewById(R.id.imgCancelForm)
             time = rootView.findViewById(R.id.image_time)
             horaInicial = rootView.findViewById(R.id.text_horas_inicial)
             horaFinal = rootView.findViewById(R.id.text_horas_final)
@@ -89,10 +91,10 @@ open class VoluntarioFragment :  Fragment()  {
                 itensSemana?.last()?.setOnClickListener(View.OnClickListener {
                     cleanSelecaoSemana()
                     val item: ImageView = it as ImageView
-                    if(itemSemanaSelectedPosition == i){
+                    if (itemSemanaSelectedPosition == i) {
                         item.setImageResource(R.drawable.button_cycle_orange)
                         itemSemanaSelectedPosition = null
-                    } else{
+                    } else {
                         item.setImageResource(R.drawable.button_cycle_blue)
                         itemSemanaSelectedPosition = i
                     }
@@ -101,31 +103,45 @@ open class VoluntarioFragment :  Fragment()  {
 
             //adding all listeners
             imgSolicitarColeta?.setOnClickListener(View.OnClickListener {
-                toggleStateCreate(true)
+                toggleStateCreate()
             })
             //clear all listeners
-            button_clear.setOnClickListener {
-                toggleStateCreate(false)
+            imgCancelForm.setOnClickListener {
+                cancelarFormulario()
             }
             //TimePicker
             time.setOnClickListener {
                 val cal = Calendar.getInstance()
-                val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                    cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                    cal.set(Calendar.MINUTE, minute)
+                val timeSetListener =
+                    TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                        cal.set(Calendar.MINUTE, minute)
 
-                    val cal1 = Calendar.getInstance()
-                    val timeSetListener1 = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                        cal1.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                        cal1.set(Calendar.MINUTE, minute)
-                        horaFinal.text = SimpleDateFormat("HH:mm").format(cal1.time)
+                        val cal1 = Calendar.getInstance()
+                        val timeSetListener1 =
+                            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                                cal1.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                                cal1.set(Calendar.MINUTE, minute)
+                                horaFinal.text = SimpleDateFormat("HH:mm").format(cal1.time)
+                            }
+                        TimePickerDialog(
+                            context,
+                            timeSetListener1,
+                            cal1.get(Calendar.HOUR),
+                            cal1.get(Calendar.MINUTE),
+                            true
+                        ).show()
+
+
+                        horaInicial.text = SimpleDateFormat("HH:mm").format(cal.time)
                     }
-                    TimePickerDialog(context, timeSetListener1, cal1.get(Calendar.HOUR), cal1.get(Calendar.MINUTE), true).show()
-
-
-                    horaInicial.text = SimpleDateFormat("HH:mm").format(cal.time)
-                }
-                TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), true).show()
+                TimePickerDialog(
+                    context,
+                    timeSetListener,
+                    cal.get(Calendar.HOUR),
+                    cal.get(Calendar.MINUTE),
+                    true
+                ).show()
 
             }
         }
@@ -138,51 +154,71 @@ open class VoluntarioFragment :  Fragment()  {
         }
     }
 
-    private fun isCamposValided(): Boolean{
-        if(itemSemanaSelectedPosition != null){
+    private fun isCamposValided(): Boolean {
+        if (itemSemanaSelectedPosition != null) {
             return true
         }
         return false
     }
 
-    private fun toggleStateCreate(visible: Boolean) {
-        if(visible){
-            if (isStateCreate == false && isStateCreated == false) {
-                YoYo.with(Techniques.RotateIn).duration(800).repeat(0).playOn(clSolicitarColeta)
-                cvCapa?.visibility = View.GONE
-                YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(clColeta)
-                tvInfoCreate?.visibility = View.VISIBLE
-                tvSpacer?.visibility = View.INVISIBLE
+    private fun toggleStateCreate() {
+        if (isCanceled == true) {
+            YoYo.with(Techniques.RotateIn).duration(800).repeat(0).playOn(clSolicitarColeta)
+            cvCapa?.visibility = View.VISIBLE
+            YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(clColeta)
+            YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(tvCapaInfo)
+            tvInfoCreate?.visibility = View.GONE
+            tvSpacer?.visibility = View.GONE
 
-                val handler = Handler()
-                val r = Runnable {
-                    imgSolicitarColeta?.setImageResource(R.drawable.icon_check_white)
-                    imgSolicitarColeta?.layoutParams?.width = 66
-                    imgSolicitarColeta?.layoutParams?.height = 66
-                }
-                handler.postDelayed(r, 700)
-                isStateCreate = true
-
-            } else if (isStateCreate == true) {
-                // varifica os campos e caso validados as opções, cria nova coleta com status "Solicitada"
-                if(isCamposValided()){
-                    var coleta = Coleta()
-                    coleta?.id_solicitante = getInstance()?.currentUser?.uid;
-                    //coleta?.id_solicitante = getInstance()?.currentUser?.uid;
-                    // pegar id_coletor do usuário voluntário
-
-                    tvSpacer?.visibility = View.GONE
-                    clChecks?.visibility = View.VISIBLE
-                    tvInfoCreate?.visibility = View.GONE
-                    YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(clColeta)
-                    YoYo.with(Techniques.RotateOut).duration(400).repeat(0).playOn(clSolicitarColeta)
-                    isStateCreated = true
-                }
+            val handler = Handler()
+            val r = Runnable {
+                imgSolicitarColeta?.setImageResource(R.drawable.icon_plus)
+                imgSolicitarColeta?.layoutParams?.width = 116
+                imgSolicitarColeta?.layoutParams?.height = 116
             }
-        }else{
-          Toast.makeText(mContext, "Sua solicitação foi cancelada", Toast.LENGTH_SHORT).show()
-        }
+            handler.postDelayed(r, 600)
+            isStateCreate = false
+            isStateCreated = false
+            isCanceled = false
 
+        } else if (isStateCreate == false && isStateCreated == false) {
+            YoYo.with(Techniques.RotateIn).duration(800).repeat(0).playOn(clSolicitarColeta)
+            cvCapa?.visibility = View.GONE
+            YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(clColeta)
+            tvInfoCreate?.visibility = View.VISIBLE
+            tvSpacer?.visibility = View.INVISIBLE
+
+            val handler = Handler()
+            val r = Runnable {
+                imgSolicitarColeta?.setImageResource(R.drawable.icon_check_white)
+                imgSolicitarColeta?.layoutParams?.width = 66
+                imgSolicitarColeta?.layoutParams?.height = 66
+            }
+            handler.postDelayed(r, 600)
+            isStateCreate = true
+
+        } else if (isStateCreate == true) {
+            // varifica os campos e caso validados as opções, cria nova coleta com status "Solicitada"
+            if (isCamposValided()) {
+                var coleta = Coleta()
+                coleta?.id_solicitante = getInstance()?.currentUser?.uid;
+                //coleta?.id_solicitante = getInstance()?.currentUser?.uid;
+                // pegar id_coletor do usuário voluntário
+
+                tvSpacer?.visibility = View.GONE
+                clChecks?.visibility = View.VISIBLE
+                tvInfoCreate?.visibility = View.GONE
+                YoYo.with(Techniques.Pulse).duration(400).repeat(0).playOn(clColeta)
+                YoYo.with(Techniques.RotateOut).duration(400).repeat(0).playOn(clSolicitarColeta)
+                isStateCreated = true
+            }
+        }
+    }
+
+    private fun cancelarFormulario() {
+        isCanceled = true
+        toggleStateCreate()
+        Toast.makeText(context, "A solicitação foi cancelada!", Toast.LENGTH_SHORT)
     }
 
     override fun onAttach(context: Context) {
