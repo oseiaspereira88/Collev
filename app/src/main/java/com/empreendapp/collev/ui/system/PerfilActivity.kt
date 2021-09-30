f0c061c587d274c501b23bdbb607dc47e0e27a1d
Oseias Pereira <oseiapereira88@gmail.com>
2021-09-30 00:39:25 -0300

    Implementado o layout da tela de perfil padrão

---
+++ app/src/main/java/com/empreendapp/collev/ui/system/PerfilActivity.kt
@@ -0,0 +1,12 @@
+package com.empreendapp.collev.ui.system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.empreendapp.collev.R
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
