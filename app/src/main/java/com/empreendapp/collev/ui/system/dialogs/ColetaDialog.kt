f3562565b11b139982c12c995aa642f72074063f
Oseias Pereira <oseiapereira88@gmail.com>
2021-10-19 01:43:10 -0300

    feat: atualização do layout do dialog_marcacao_coleta.xml

--- app/src/main/java/com/empreendapp/collev/ui/system/dialogs/ColetaDialog.kt
+++ app/src/main/java/com/empreendapp/collev/ui/system/dialogs/ColetaDialog.kt
@@ -11,13 +11,13 @@ class ColetaDialog: DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.transparent_bg)
-        return inflater.inflate(R.layout.dialog_coleta_view, container, false)
+        return inflater.inflate(R.layout.dialog_marcacao_coleta, container, false)
     }
 
     override fun onStart() {
         super.onStart()
-        val width = (resources.displayMetrics.widthPixels * 1).toInt()
-//        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+        val width = (resources.displayMetrics.widthPixels * 0.86).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
         dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
